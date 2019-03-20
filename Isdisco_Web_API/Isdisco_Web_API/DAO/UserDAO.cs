d935eedfe7ee1c8a2b1afa4f11d6f4167cb2e172
MagnusKochDTU <s175189@student.dtu.dk>
2019-03-20 15:32:50 +0100

    Added servies and some models for the services

---
+++ Isdisco_Web_API/Isdisco_Web_API/DAO/UserDAO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Isdisco_Web_API.DAO
+{
+    public class UserDAO
+    {
+        public UserDAO()
+        {
+        }
+    }
+}
---
+++ Isdisco_Web_API/Isdisco_Web_API/Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Isdisco_Web_API.Models
+{
+    public class Admin
+    {
+        public Admin()
+        {
+        }
+    }
+}
