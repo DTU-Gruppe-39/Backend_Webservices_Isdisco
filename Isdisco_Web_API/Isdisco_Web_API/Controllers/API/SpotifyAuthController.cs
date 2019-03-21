46625f2ca584a636fb122a5d50e736997adc1a96
rallerG <raller799@live.dk>
2019-03-21 10:14:55 +0100

    Setup but did not test spotify auth

---
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/API/SpotifyAuthController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace Isdisco_Web_API.Controllers.API
+{
+    public class SpotifyAuthController
+    {
+        public SpotifyAuthController()
+        {
+        }
+
+       
+         //see https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+        public void GetClientCredentialsAuthToken()
+        {
+            var spotifyClient = "80b3eb68b8454a0c81554c61b47bcc39";
+            var spotifySecret = "c2a18bfa0793457f8a59d277ba412425";
+
+            var webClient = new WebClient();
+
+            var postparams = new NameValueCollection();
+            postparams.Add("grant_type", "client_credentials");
+
+            var authHeader = Convert.ToBase64String(Encoding.Default.GetBytes($"{spotifyClient}:{spotifySecret}"));
+            webClient.Headers.Add(HttpRequestHeader.Authorization, "Basic " + authHeader);
+
+            var tokenResponse = webClient.UploadValues("https://accounts.spotify.com/api/token", postparams);
+
+            var textResponse = Encoding.UTF8.GetString(tokenResponse);
+        }
+
+    }
+}
---
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/API/SpotifyTrackService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Isdisco_Web_API.Controllers.API
+{
+
+    [Route("api/spotify-track")]
+    public class SpotifyTrackService : Controller
+    {
+        // GET: api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Spotify is nice";
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
