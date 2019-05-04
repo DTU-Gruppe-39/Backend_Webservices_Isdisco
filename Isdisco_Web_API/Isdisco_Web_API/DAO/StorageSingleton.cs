9ce28fcd3f61808ae338622c541d6189748a4c55
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-04 20:35:24 +0200

    Update StorageSingleton.cs

--- Isdisco_Web_API/Isdisco_Web_API/DAO/StorageSingleton.cs
+++ Isdisco_Web_API/Isdisco_Web_API/DAO/StorageSingleton.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using Isdisco_Web_API.Models;
+using Isdisco_Web_API.Controllers.Businesslogic;
 
 namespace Isdisco_Web_API.DAO
 {
     public class StorageSingleton
     {
         static StorageSingleton Singleton = null;
-
+        public JwtFromP8 p8 = new JwtFromP8();
 
         private List<User> userList = new List<User>();
         public List<User> UserList 
@@ -49,13 +50,15 @@ namespace Isdisco_Web_API.DAO
         {
             UserList.Add(new User("Rasmus Gregersen", new LoginDetails("rasmus", "123"), false, "F9659290C65335689BB1F300EDDFDEC036BB3D32E78DB726879049AD487B333D"));
             UserList.Add(new User("Thomas Mattsson", new LoginDetails("thomas", "123"), false, "834A1C6138CD293AC464D6CBFDBC987C3F73BC691EF55702F6DE5E84F2DA7081"));
-        }
+            string jwtToken = p8.GetToken();
+    }
 
-        public static StorageSingleton GetInstance()
+    public static StorageSingleton GetInstance()
         {
             if (Singleton == null)
             {
                 Singleton = new StorageSingleton();
+
                 //Adds token to list
                 //Singleton.InitDevicePushTokenList();
                 return Singleton;
