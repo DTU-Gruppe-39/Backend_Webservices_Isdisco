64ee9c1f676ca206c84b1b818b1790e2c59a28b6
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-04 12:25:02 +0200

    Started push logic

--- Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/NotificationControllerClass.cs
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/NotificationControllerClass.cs
@@ -18,7 +18,7 @@ namespace Isdisco_Web_API.Controllers.Businesslogic
         {
         }
 
-        public async System.Threading.Tasks.Task SendNotificationAsync()
+        public async System.Threading.Tasks.Task SendNotificationAsync(string title, string msg, Models.DevicePushToken)
         {
             await apnhttp.SendAsync("Hej", deviceToken, p8.GetToken(), false);
         }
