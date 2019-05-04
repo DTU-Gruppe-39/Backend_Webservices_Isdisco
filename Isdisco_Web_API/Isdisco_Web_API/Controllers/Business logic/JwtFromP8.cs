7ba89a3a2a49ea3fe62406d00f2162d73ee0f7ee
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-04 11:10:24 +0200

    Update JwtFromP8.cs

--- Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/JwtFromP8.cs
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/JwtFromP8.cs
@@ -35,9 +35,7 @@ namespace Isdisco_Web_API.Controllers.Businesslogic
             var p8Path = Path.Combine(outPutDirectory, "push-cert.p8");
             string p8_path = new Uri(p8Path).LocalPath;
             */
-            using (var stream = GenerateStreamFromString("a,b \n c,d"))
-            {
-                // ... Do stuff to stream
+          
 
                 using (TextReader reader = new StreamReader("-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgzdgv9ENf8lc74VfU\n1jCn4WEXryur2sOK6tXBfWnNJGigCgYIKoZIzj0DAQehRANCAARH8kCLw2xvoDGl\njoRv2CWGi6xo8ygK6VYrFCq6TbKyvQksKlsbVoqsmDB3N8f0c3xOsktvYxNtaUf3\nUUHcMXs8\n-----END PRIVATE KEY-----"))
                 {
@@ -52,7 +50,6 @@ namespace Isdisco_Web_API.Controllers.Businesslogic
                     return ECDsa.Create(msEcp);
                 }
             }
-        }
 
         private string CreateJwt(ECDsa key, string keyId, string teamId)
         {
