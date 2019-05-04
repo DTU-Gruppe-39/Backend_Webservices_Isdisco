1ae0c975051173df89144598084d1a600ec54ccc
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-04 09:55:57 +0200

    Update JwtFromP8.cs

--- Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/JwtFromP8.cs
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/Business logic/JwtFromP8.cs
@@ -35,7 +35,7 @@ namespace Isdisco_Web_API.Controllers.Businesslogic
             var p8Path = Path.Combine(outPutDirectory, "push-cert.p8");
             string p8_path = new Uri(p8Path).LocalPath;
             */
-            using (TextReader reader = new StringReader("MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgzdgv9ENf8lc74VfU1jCn4WEXryur2sOK6tXBfWnNJGigCgYIKoZIzj0DAQehRANCAARH8kCLw2xvoDGljoRv2CWGi6xo8ygK6VYrFCq6TbKyvQksKlsbVoqsmDB3N8f0c3xOsktvYxNtaUf3UUHcMXs8"))
+            using (TextReader reader = new StringReader("MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgzdgv9ENf8lc74VfU\n1jCn4WEXryur2sOK6tXBfWnNJGigCgYIKoZIzj0DAQehRANCAARH8kCLw2xvoDGl\njoRv2CWGi6xo8ygK6VYrFCq6TbKyvQksKlsbVoqsmDB3N8f0c3xOsktvYxNtaUf3\nUUHcMXs8"))
             {
                 var ecPrivateKeyParameters =
                     (ECPrivateKeyParameters)new Org.BouncyCastle.OpenSsl.PemReader(reader).ReadObject();
