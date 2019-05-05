663d8e6b7628c58f733f7ac219391b2de935ff64
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-05 16:45:13 +0200

    Started on feedback API

---
+++ Isdisco_Web_API/Isdisco_Web_API/Controllers/API/FeedbackService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Isdisco_Web_API.Controllers.Businesslogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Isdisco_Web_API.Controllers.API
+{
+
+    [Route("api/feedback")]
+    public class FeedbackService
+    {
+
+        Businesslogic.FeedbackController fc = new FeedbackController();
+
+        [HttpPost("upload")]
+        public void PostFeedback()
+        {
+
+        }
+
+    }
+}
