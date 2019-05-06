10652c77373964feb1cf3d445fe458c1e11d287c
Thomas Mattsson <tmattsson0@gmail.com>
2019-05-06 17:03:17 +0200

    Update FeedbackDAO.cs

--- Isdisco_Web_API/Isdisco_Web_API/DAO/FeedbackDAO.cs
+++ Isdisco_Web_API/Isdisco_Web_API/DAO/FeedbackDAO.cs
@@ -47,7 +47,6 @@ namespace Isdisco_Web_API.DAO
 
         public List<Feedback> GetTag(string tag)
         {
-            //Todo Implement method
             List<Feedback> taggedFeedbackList = new List<Feedback>();
             foreach (Feedback feedback in storageSingleton.FeedbackList)
             {
