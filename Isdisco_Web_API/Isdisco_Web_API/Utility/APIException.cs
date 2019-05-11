3bf34b2dd9b744ad2311aaf0aa85cede2271c457
MagnusKochDTU <s175189@student.dtu.dk>
2019-05-11 19:48:34 +0200

    API exception class

---
+++ Isdisco_Web_API/Isdisco_Web_API/Utility/APIException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace Isdisco_Web_API.Utility
+{
+    public class APIException
+    {
+        private HttpStatusCode StatusCode { get; set; }
+        private string Type { get; set; } = @"text/plain";
+
+        public APIException(HttpStatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public APIException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public APIException(HttpStatusCode statusCode, Exception inner) : this(statusCode, inner.ToString()) 
+        { 
+            
+        }
+
+        public APIException(HttpStatusCode statusCode, Newtonsoft.Json.Linq.JObject errorObject) : this(statusCode, errorObject.ToString())
+        {
+            this.Type = @"application/json";
+        }
+    }
+}
